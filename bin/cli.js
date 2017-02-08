0a923fb2b60fae2db2bf59ff013c39deb9d57ae8
Maximilian Antoni <mail@maxantoni.de>
2017-02-08 22:05:07 +0100

    Use `@studio/log` and `ora` spinner

--- bin/cli.js
+++ bin/cli.js
@@ -10,7 +10,10 @@ const url = require('url');
 const zlib = require('zlib');
 const { PassThrough } = require('stream');
 const minimist = require('minimist');
+const ora = require('ora');
 const convert_source_map = require('convert-source-map');
+const logger = require('@studio/log');
+const log_format = require('@studio/log/format/fancy');
 const studio_config = require('../lib/config');
 const upload = require('../lib/upload');
 const load_report = require('../lib/load-report');
@@ -37,27 +40,41 @@ if (argv.version) {
   return;
 }
 
+logger.transform(log_format({ ts: false, ns: false }));
+if (!argv.debug) {
+  logger.mute('json-request', 'fetch');
+}
+
 let stream_end = false;
 let config = null;
 let url_json = null;
 let gzip_buffer = null;
 let source_map;
 
-function fail(message) {
-  console.error(` 🚨  ${message}`);
+console.log('');
+const spinner = ora('').start();
+
+function fail(message, error) {
+  spinner.stop();
+  logger('cli').error(message, {}, error);
   process.exitCode = 1;
 }
 
 function upload_gzip() {
+  if (argv.debug) {
+    spinner.stop();
+  } else {
+    spinner.text = 'Uploading';
+  }
   const stream = new PassThrough();
   upload.upload(url_json.url, stream, gzip_buffer.length, (err) => {
     if (err) {
-      fail(`Failed to upload file: ${err.message}`);
+      fail('Failed to upload file', err.message);
       return;
     }
-    load_report(config, url_json.number, argv, (err, report_json) => {
+    load_report(config, url_json.number, argv, spinner, (err, report_json) => {
       if (err) {
-        fail(`Failed to load report: ${err.message}`);
+        fail('Failed to load report', err.message);
         return;
       }
       render_report(report_json, source_map);
@@ -92,6 +109,8 @@ function read_stream(stream) {
       stream_end = true;
       if (url_json) {
         upload_gzip();
+      } else {
+        spinner.text = 'Pending upload';
       }
     });
     gzip.write(source);
@@ -99,11 +118,12 @@ function read_stream(stream) {
   });
 }
 
+spinner.text = 'Reading input';
 if (argv.file) {
   const stream = fs.createReadStream(argv.file, 'utf8');
   stream.on('error', (err) => {
     if (err) {
-      fail(`Failed to read file ${argv.file}: ${err.message}`);
+      fail(`Failed to read file ${argv.file}`, err.message);
       return;
     }
   });
@@ -117,7 +137,7 @@ const config_file = path.join(studio_config.home(), '.studio', 'config');
 
 studio_config.read(config_file, (err, values) => {
   if (err) {
-    fail(`Failed to read config: ${err.message}`);
+    fail('Failed to read config', err.message);
     return;
   }
   const api = values.api || 'https://api.javascript.studio/beta';
@@ -132,6 +152,10 @@ studio_config.read(config_file, (err, values) => {
     token: values.token
   };
 
+  if (argv.debug) {
+    spinner.stop();
+  }
+
   let data = null;
   if (argv.global) {
     data = {
@@ -140,12 +164,14 @@ studio_config.read(config_file, (err, values) => {
   }
   upload.url(config, data, (err, json) => {
     if (err) {
-      fail(`Failed to get upload URL: ${err.message}`);
+      fail('Failed to get upload URL', err.message);
       return;
     }
     url_json = json;
     if (stream_end) {
       upload_gzip();
+    } else if (argv.debug) {
+      spinner.start().text = 'Reading input';
     }
   });
 });
