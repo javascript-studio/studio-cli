b3df53906e59e7b0557260a3ce9eedf0f40aad09
Maximilian Antoni <mail@maxantoni.de>
2017-05-29 18:04:56 +0200

    Allow to specify the token and secret in environment variables

--- test/config-builder-test.js
+++ test/config-builder-test.js
@@ -2,9 +2,19 @@
 'use strict';
 
 const assert = require('assert');
+const sinon = require('sinon');
 const config_builder = require('../lib/config-builder');
 
-describe('config-builder', () => {
+describe.only('config-builder', () => {
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
 
   it('returns default API', () => {
     const config = config_builder.build({
@@ -16,7 +26,8 @@ describe('config-builder', () => {
       hostname: 'api.javascript.studio',
       port: null,
       basepath: '/beta',
-      token: '123-456'
+      token: '123-456',
+      secret: null
     });
   });
 
@@ -31,8 +42,37 @@ describe('config-builder', () => {
       hostname: 'localhost',
       port: '1337',
       basepath: '/test',
+      token: '123-456',
+      secret: null
+    });
+  });
+
+  it('uses token from environment', () => {
+    sandbox.stub(process.env, 'STUDIO_TOKEN').value('789-000');
+
+    const config = config_builder.build({
       token: '123-456'
     });
+
+    assert.equal(config.token, '789-000');
+  });
+
+  it('uses secret from config', () => {
+    const config = config_builder.build({
+      secret: 'abcdef'
+    });
+
+    assert.equal(config.secret, 'abcdef');
+  });
+
+  it('uses secret from environment', () => {
+    sandbox.stub(process.env, 'STUDIO_SECRET').value('123456');
+
+    const config = config_builder.build({
+      secret: 'abcdef'
+    });
+
+    assert.equal(config.secret, '123456');
   });
 
 });
